3352e84d7531bc12ed847874874eb0766512f16d
ferhatgec <ferhatgectao@gmail.com>
2020-12-10 02:06:31 +0300

    Add: HelpFunction() && version.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+const VERSION: &str = "0.1-beta-1";
+
 #[allow(non_snake_case)]
 fn Read<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -40,11 +42,27 @@ fn ReadFile(filename: &str) {
 	}		
 }
 
+#[allow(non_snake_case)]
+fn HelpFunction(argument: &str) {
+	println!("{}\n{}\n{}", 
+		format!("{}Fegeya {}Rustocat {}{}", 
+			languages::WBOLD_RED_COLOR, 
+			languages::WBOLD_GREEN_COLOR,
+			languages::WBOLD_BLUE_COLOR,
+			&VERSION).as_str(),
+		format!("{} Colorized 'cat' implementation.", &languages::WBOLD_YELLOW_COLOR).as_str(),
+		format!("{}{} [file]{}", 
+			languages::WBOLD_LIGHT_MAGENTA_COLOR, 
+			argument,
+			languages::WRESET).as_str());
+
+}
+
 fn main() {
 	let file: Vec<_> = std::env::args().collect();
 	
 	if file.len() < 2 {
-		println!("Fegeya RustoCat\n{} <file>", &file[0]);
+		HelpFunction(&file[0]);
 		std::process::exit(0);
 	}
 	
